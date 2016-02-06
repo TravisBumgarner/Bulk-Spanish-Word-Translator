c4e6997cb15a0535bcd8df69e660158103de2725
Travis Bumgarner <travis.bumgarner@gmail.com>
2016-02-06 16:53:49 -0500

    V3.1
    Working on dealing with various search result types from SpanishDict.com

--- spanishDict.py
+++ spanishDict.py
@@ -71,10 +71,12 @@ def findWords(wordListToSearch):
     return searchResults
     
 def spanishDictSearch(word):
+    #requires bs4, requests
     res = requests.get('http://www.spanishdict.com/translate/' + word)
     noStarchSoup = bs4.BeautifulSoup(res.text, "html.parser")
     result = noStarchSoup.select('.lang .el')
     concatWord = ""
+    #Following code deals with how SpaishDict presents words such as El Gato nested inside two different link tags.
     for i in range(len(result)):
         concatWord += result[i].getText()
         if(i < len(result)-1):
--- spanishDictTestEnvironment.py
+++ spanishDictTestEnvironment.py
@@ -1,58 +1,16 @@
-import csv, shutil, os
-
-def newDictionary(dict):
-    #requires os module
-    fileName, fileExtension = os.path.splitext(dict)
-    if(os.path.isfile(dict)):
-        print(dict + " already exists. Please enter a unique file name")
-    elif(fileExtension != ".csv"):
-        print(dict + " does not have .csv file extension.")
+def spanishDictSearch(wordSearched):
+    import bs4, requests
+    res = requests.get('http://www.spanishdict.com/translate/' + wordSearched)
+    noStarchSoup = bs4.BeautifulSoup(res.text, "html.parser")
+    result = noStarchSoup.select('.lang .el')
+    wordFound = ""
+    if(result[0].getText() == "" || #Check SA to figure out of reslt[0] is empty#):
+        wordFound = "notFound"
     else:
-        newDict = open(dict,'w')
-
-def openDictionary(fileName):
-    #requires csv module
-    dictionaryFile = open(fileName)
-    dictionaryReader = csv.reader(dictionaryFile)
-    dictionaryData = list(dictionaryReader)
-    return "Dictionary " + fileName + " was successfully opened. It's contents are " + str(dictionaryData)
-
-def saveDictionary():
-    #I have no idea what this will do
-    print("Yay!")
-
-def appendDictionary():
-    #I have no idea what this will do
-    print("Yay!")
-
-def backupDictionary(fileToBackup):
-    #requires os
-    backupFolder = ".\dictBackup"
-    fileName, fileExtension = os.path.splitext(fileToBackup)
-
-    i = 0
-    newFileName = fileName + str(i) + fileExtension
-
-    for folderName, subfolders, filenames in os.walk(backupFolder):
-        while newFileName in filenames:
-            i +=1 
-            newFileName = fileName + str(i) + fileExtension
-            if newFileName not in filenames:
-                break
-        return newFileName
-   
-def searchDictionary(word,dictionary,col):
-    #Which word to search for
-    #Which dictionary to search in
-    # Which column to search through
-        # 0 = Spanish
-        # 1 = English
-        # 2-end to be defnied
-    colToSearch = []
-    for entry in dictionary:
-        colToSearch.append(entry[col])
-    if word in colToSearch:
-        print("found")
-    else:
-        print("not found")
-    
+        #Following code deals with how SpaishDict presents words such as El Gato nested inside two different link tags.
+        for i in range(len(result)):
+            wordFound += result[i].getText()
+            if(i < len(result)-1):
+                #Adds a space between each word unless the last word is reached
+                wordFound += " "
+    return wordFound
