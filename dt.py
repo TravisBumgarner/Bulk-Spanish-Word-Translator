abbf1205362c6c4f892405ebbc7630a565141290
Travis Bumgarner <travis.bumgarner@gmail.com>
2016-02-13 13:25:56 -0500

    V0.5
    Converted dicttools module to class.
    I've learned classes!

--- dt.py
+++ dt.py
@@ -1,66 +1,63 @@
-import csv, shutil, os
+import pickle #used for file handling
+import os #used for backing up files
 
-def newD():
-    return {}
-
-def openD(fileName):
-    #requires csv module
-    dictionaryFile = open(fileName)
-    dictionaryReader = csv.reader(dictionaryFile)
-    dictionaryData = list(dictionaryReader)
-    return "Dictionary " + fileName + " was successfully opened. It's contents are " + str(dictionaryData)
-
-def saveD():
-    #I have no idea what this will do
-    print("Yay!")
-    #dictLayout = {'spanishWord':['eng word',['forms','seem','of','word'],'timesSeen']}
-
-def appendListD(dictionary, spanishWordList, englishWordList, seenWordList):
-    #adds new entries found to dictionary
-    #don't set equation equal to anything. Takes in dictionary and returns 
-    def appendD(dictionary,spanishWord,englishWord,seenWord):
-        timesSeen = 1
-        dictionary[spanishWord] = [englishWord, [seenWord], timesSeen]
-        return dictionary
+class dt():
+    def __init__(self):
+        #Starts with a blank dictionary
+        self.dictionary = {}
         
-    for index in range(len(spanishWordList)):
-        appendD(dictionary, spanishWordList[index], englishWordList[index], seenWordList[index])
-    
-def backupD(fileToBackup):
-    #requires os
-    backupFolder = ".\dictBackup"
-    fileName, fileExtension = os.path.splitext(fileToBackup)
+    def openD(self,fileName):
+        #Use pickle to open file
+        try:
+            self.dictionary = pickle.load(open(fileName,"rb"))
+        except FileNotFoundError:
+            print("File does not exist.")
+        except pickle.UnpicklingError:
+            print("Not valid pickle file.")
 
-    i = 0
-    newFileName = fileName + str(i) + fileExtension
-
-    for folderName, subfolders, filenames in os.walk(backupFolder):
-        while newFileName in filenames:
-            i +=1 
-            newFileName = fileName + str(i) + fileExtension
-            if newFileName not in filenames:
-                break
-        return newFileName
-   
-def searchD(word,dictionary,col):
-    #Which word to search for
-    #Which dictionary to search in
-    # Which column to search through
-        # 0 = Spanish
-        # 1 = English
-        # 2-end to be defnied
-    colToSearch = []
-    for entry in dictionary:
-        colToSearch.append(entry[col])
-    if word in colToSearch:
-        print("found")
-    else:
-        print("not found")
-
-def printPrettyD(dictionary):
-    for i in dictionary:
-        stringOfSeen = ""
-        for j in dictionary[i][1]:
-            stringOfSeen += " " + j
-        print("The word \"" + i + "\" in English is \"" + dictionary[i][0] + "\". It has been seen " + str(dictionary[i][2]) + " times, as" + stringOfSeen + ".")
+    def saveD(self,fileName):
+        #Use pickle to save file
+        pickle.dump(self.dictionary,open(fileName,"wb"))
         
+    def appendD(self,spanishWordList,englishWordList, seenWordList):
+        def appendEachD(spanishWord,englishWord,seenWord):
+            timesSeen = 1
+            self.dictionary[spanishWord] = [englishWord, [seenWord],timesSeen]
+            #return dictionary
+            #pretty sure I don't need this anymore
+        for index in range(len(spanishWordList)):
+            appendEachD(spanishWordList[index],
+                        englishWordList[index],
+                           seenWordList[index])
+
+    def backupD(self,fileName):
+        backupFolder = ".\dictBackup"
+        #Makes backup folder if it doesn't exist
+        if not os.path.exists(backupFolder):
+            os.makedirs(backupFolder)
+
+        #Used for creating unique filenames for each time saved
+        def genFileName(fileName):
+            fname, fextension = os.path.splitext(fileName)
+            i = 0
+            newFileName = fname + str(i) + fextension
+            for folderName,subfolders, filenames in os.walk(backupFolder):
+                while newFileName in filenames:
+                    i += 1
+                    newFileName = fname + str(i) + fextension
+                    if newFileName not in filenames:
+                        break
+                return newFileName
+        pickle.dump(self.dictionary,open(backupFolder + "\\" + genFileName(fileName),"wb"))
+
+    def searchD(self,word):
+
+        try:
+            #Creates list of forms seen of the word.
+            formsSeenStr = ', '.join(self.dictionary[word][1])
+            print("The word \"" + word
+                  + "\" in English is \"" + self.dictionary[word][0]
+                  + "\". It has been seen " + str(self.dictionary[word][2])
+                  + " times, as "+ formsSeenStr + ".")
+        except KeyError:
+            print("The word " + word + " was not found in the dictionary.")               
--- testingFolder/classes.py
+++ testingFolder/classes.py
@@ -1,4 +1,6 @@
 import pickle #used for file handling
+import os #used for backing up files
+
 class dt():
     def __init__(self):
         #Starts with a blank dictionary
@@ -27,13 +29,45 @@ class dt():
             appendEachD(spanishWordList[index],
                         englishWordList[index],
                            seenWordList[index])
-            
+
+    def backupD(self,fileName):
+        backupFolder = ".\dictBackup"
+        #Makes backup folder if it doesn't exist
+        if not os.path.exists(backupFolder):
+            os.makedirs(backupFolder)
+        #Used for creating unique filenames for each time saved
+        def genFileName(fileName):
+            fname, fextension = os.path.splitext(fileName)
+            i = 0
+            newFileName = fname + str(i) + fextension
+            for folderName,subfolders, filenames in os.walk(backupFolder):
+                while newFileName in filenames:
+                    i += 1
+                    newFileName = fname + str(i) + fextension
+                    if newFileName not in filenames:
+                        break
+                return newFileName
+        pickle.dump(self.dictionary,open(backupFolder + "\\" + genFileName(fileName),"wb"))
+
+    def searchD(self,word):
+
+        try:
+            #Creates list of forms seen of the word.
+            formsSeenStr = ', '.join(self.dictionary[word][1])
+            print("The word \"" + word
+                  + "\" in English is \"" + self.dictionary[word][0]
+                  + "\". It has been seen " + str(self.dictionary[word][2])
+                  + " times, as "+ formsSeenStr + ".")
+        except KeyError:
+            print("The word " + word + " was not found in the dictionary.")
+   
 
 
 #Run Tests               
 new = dt()
 test = ["cat","dog"]
-new.appendD(test,test,test)        
+new.appendD(test,test,test)
+new.searchD("cat")
                                           
         
                                       
