ed16f48b63067d979dd8af9bf873773e07201ead
Travis Bumgarner <travis.bumgarner@gmail.com>
2016-02-13 15:19:14 -0500

    V0.61
    Save before attempting to scrape SpanishDict for spanish word.

--- spanishDict.py
+++ spanishDict.py
@@ -1,5 +1,6 @@
 import requests, bs4, os, urllib
 from dictTools import dt #My created tools for working with dictionaries
+
 '''
 Spanishdict.com search for "palabra" returns:
 http://www.spanishdict.com/translate/palabras
@@ -29,47 +30,39 @@ def intro():
 
 ################################################
 ########## Methods for inputting words #########
-def createFromText():
-    print("Enter each word followed by pressing enter.")
-    print("Once you're done entering words type 'AllDone'")
-    wordsList = []
-    word = ''
-    while(word != "AllDone"):
-        word = input()
-        if(word!="AllDone"):
-            wordsList.append(word)
-    return wordsList
-
-def createFromFile():
-    #Reads wordlist form txt
-    #requires os module
-    wordListLocation = ''
-    while(not os.path.isfile(wordListLocation)):
+class add:
+    def fromText():
+        print("Enter each word followed by pressing enter.")
+        print("Once you're done entering words type 'AllDone'")
+        wordsList = []
+        word = ''
+        while(word != "AllDone"):
+            word = input()
+            if(word!="AllDone"):
+                wordsList.append(word)
+        if len(wordsList) != 0:
+            return wordsList
+        else:
+            print("No words provided. Returning None.")
+            return None
+        
+    def fromFile(fileName):
+        #Reads wordlist form txt
+        #requires os module
         try:
-            wordListLocation = input('Specify txt file to import:\n')
-            wordListLocationOpen = open(wordListLocation)
-            wordListLocationRead = wordListLocationOpen.read()
+            fileNameOpen = open(fileName)
+            fileNameRead = fileNameOpen.read()
             #wordList = wordListLocation.read()
+            return fileNameRead.split('\n')
         except FileNotFoundError:
             print("Enter a valid file location")
         except PermissionError:
             print("Make sure to specify the file name and file path.")
-        return wordListLocationRead.split('\n')
 
-def findEnglishWords(wordListToSearch):
-    searchResults = []
-    if(type(wordListToSearch) == str):
-        #If wordList is single string, convert to list
-        wordListToSearch = [wordListToSearch]
+###################################
+### Translate Class will go Here###
+###################################
 
-    if(not check_connectivity('http://74.125.224.72/')):
-        #URL For Google to see if internet is working
-        print('Please check internet connection and try again')
-    else:
-        for word in wordListToSearch:
-            searchResults.append(spanishDictSearch(word))
-    return searchResults
-    
 def spanishDictSearch(word):
     #requires bs4, requests
     res = requests.get('http://www.spanishdict.com/translate/' + word)
@@ -83,6 +76,25 @@ def spanishDictSearch(word):
             #Adds a space between each word unless the last word is reached
             concatWord += " "
     return concatWord
+
+def spanishToEnglish(wordListToSearch):
+    searchResults = []
+    if(type(wordListToSearch) == str):
+        #If wordList is single string, convert to list
+        wordListToSearch = [wordListToSearch]
+            
+    if(not check_connectivity('http://74.125.224.72/')):
+        #URL For Google to see if internet is working
+        print('Please check internet connection and try again')
+    else:
+        for word in wordListToSearch:
+            if(spanishDictSearch(word)==""):
+                word = input(word + " not found. Enter word again or type 'skipWord' to continue.\n")
+                if(word == "skipWord"):
+                    continue                    
+            searchResults.append(spanishDictSearch(word))
+                
+    return searchResults
     
 def check_connectivity(reference):
     #requires urllib
@@ -92,9 +104,9 @@ def check_connectivity(reference):
     except urllib.request.URLError:
         return False
 
-spanish = createFromText()
-english = findEnglishWords(spanish)
-seen = spanish #change this once I've had a way to track orginal input
-dictionary = dictTools.newD()
-dictTools.appendListD(dictionary,spanish,english,seen)
-print(dictionary)
+def runFunction():
+    intro()
+    seenSpanish = add.fromText()
+    english = spanishToEnglish(seenSpanish)
+    print(results)
+runFunction()
---
+++ testingFolder/testst.py
@@ -0,0 +1,5 @@
+class Test:
+    def method(self):
+        print("hello")
+    def method2(self):
+        self.method()
